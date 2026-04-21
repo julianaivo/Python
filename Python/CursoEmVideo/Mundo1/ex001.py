671f2129fb6735853e0fd3af8c13ebb49ad156ff
Juliana Ivo <julianassivo@gmail.com>
2026-04-21 12:18:54 -0300

    adicionando exercício 001

---
+++ Python/CursoEmVideo/Mundo1/ex001.py
@@ -0,0 +1,10 @@
+'''Crie um programa que escreva "Olá, Mundo!" na tela.'''
+
+print('Olá, Mundo!')
+
+''' ou poderia ser escrito assim, com o uso de uma variável:
+
+msg = 'Olá, Mundo!'
+print(msg)
+
+'''
