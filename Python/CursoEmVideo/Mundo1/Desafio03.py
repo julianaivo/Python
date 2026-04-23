258d7661a6e17ab3142deeac6e878e0415b87616
Juliana Ivo <julianassivo@gmail.com>
2026-04-23 17:00:02 -0300

    adicionando explicação ao código

--- Python/CursoEmVideo/Mundo1/Desafio03.py
+++ Python/CursoEmVideo/Mundo1/Desafio03.py
@@ -1,5 +1,7 @@
 '''Crie um script python que leia dois números e tente mostrar a soma entre eles.'''
 
+#se não colocar o int() na frente do input, o programa irá concatenar os números ao invés de somá-los, pois o input() retorna uma string. Exemplo: se o usuário digitar 2 e 3, o resultado será 23 ao invés de 5.
+
 n1 = int(input('Primeiro número: '))
 n2 = int(input('Segundo número: '))
 
