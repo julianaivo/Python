c9cff568f0568f64a29383cdcf888bd88a5cfd12
Juliana Ivo <julianassivo@gmail.com>
2026-04-27 16:24:31 -0300

    média aritmética

---
+++ Python/CursoEmVideo/Mundo1/ex007.py
@@ -0,0 +1,8 @@
+'''Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.'''
+
+n1 = float(input('Primeira nota do aluno: '))
+n2 = float(input('Segunda nota do aluno: '))
+
+media = (n1 + n2)/2
+
+print('A média entre {} e {} é igual a {:.1f}.'.format(n1, n2, media)) #média com apenas uma casa decimal após a vírgula
