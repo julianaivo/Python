af3d95bf42ba41ad5b4b0216c226ac53113156e6
Juliana Ivo <julianassivo@gmail.com>
2026-04-30 12:36:57 -0300

    sorteando uma ordem na lista

---
+++ Python/CursoEmVideo/Mundo1/ex020.py
@@ -0,0 +1,13 @@
+'''O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.'''
+
+import random 
+
+n1 = input('Primeiro nome: ')
+n2 = input('Segundo nome: ')
+n3 = input('Terceiro nome: ')
+n4 = input('Quarto nome: ')
+
+lista = [n1, n2, n3, n4]
+random.shuffle(lista)
+
+print('A ordem de apresentação do trabalho será: ', lista)
